3cb99f83bd7cd96264addcc730410afd1940d35c
Pierre Roumieux <p.roumieux87@gmail.com>
2017-12-22 16:04:17 +0100

    Ajout CSS + FOSUB Config

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
             new PR\VitrineBundle\PRVitrineBundle(),
+            new PR\UserBundle\PRUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
---
+++ src/PR/UserBundle/Entity/User.php
@@ -0,0 +1,22 @@
+<?php
+// src/PR/UserBundle/Entity/User.php
+
+namespace PR\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+
+/**
+ * @ORM\Table(name="vitrine_user")
+ * @ORM\Entity(repositoryClass="PR\UserBundle\Repository\UserRepository")
+ */
+class User extends BaseUser
+{
+  /**
+   * @ORM\Column(name="id", type="integer")
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  protected $id;
+}
