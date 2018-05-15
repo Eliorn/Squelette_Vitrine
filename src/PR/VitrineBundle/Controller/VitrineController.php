487869bca798f4e62bff29921445e369b17350f9
Pierre Roumieux <p.roumieux87@gmail.com>
2018-05-15 09:06:59 +0200

    Maj Admin LTE

--- src/PR/VitrineBundle/Controller/VitrineController.php
+++ src/PR/VitrineBundle/Controller/VitrineController.php
@@ -17,11 +17,74 @@ class VitrineController extends Controller
     return new Response($content);
   }
 
-  public function gallerieAction()
+  public function carouselAction()
   {
-    $content = $this->get('templating')->render('PRVitrineBundle:Vitrine:gallerie.html.twig');
+    $content = $this->get('templating')->render('PRVitrineBundle:Vitrine:carrousel.html.twig');
 
     return new Response($content);
+  }
+
+  public function gallerieAction()
+  {
+
+    $dir=$_SERVER['DOCUMENT_ROOT']."/web_vitrine/web/uploads";
+      // echo '<pre>';
+
+      //on parcour le dossier pour les titres des fichiers
+      if($dossier = @opendir($dir)){
+        while(($fichier = readdir($dossier))!==false ){
+          if($fichier != '.' && $fichier != '..'){
+
+          }
+        }
+      }
+      // echo '</pre>';
+      //on parcour le dossier pour récupérer le contenu des fichiers
+
+      $htmlToShow='<table id="table" class="table table-striped table-bordered table-hover">
+        <caption> Fichiers déposés </caption>
+        <thead>
+          <tr>
+          <th colspan="1" rowspan="1"  tabindex="1">Fichier</th>
+          <th colspan="1" rowspan="1"  tabindex="2">Lien vers le fichier </th>
+          <th colspan="1" rowspan="1"  tabindex="3">Date de dépot </th>
+          </tr>
+        </thead>
+        <tbody>	';
+      if($dossier = @opendir($dir)){
+
+        while(false !== ($fichier = readdir($dossier))){
+
+
+          if($fichier != '.' && $fichier != '..'){
+            $dirComplet=$_SERVER['DOCUMENT_ROOT']."/web_vitrine/web/uploads/files/";
+
+
+            if($dossier2 = @opendir($dirComplet)){
+
+              while(($fichier2 = readdir($dossier2))!==false ){
+                if (!is_dir($dirComplet.$fichier2)){
+                  if($fichier2 != '.' && $fichier2 != '..'){
+
+                    $htmlToShow.="<tr>
+                        <td> $fichier</td>
+                        <td> <a target='_blank' href='.".($this->getParameter('dir_uploads')."files/$fichier2")."'>$fichier2</a> </td>
+                        <td> ".date ('d/m/Y H:m:s', filemtime($dirComplet.$fichier2))."</td>
+                      </tr>";
+
+                    clearstatcache();
+                  }
+                }
+              }
+            }
+          }
+
+        }
+      }
+      $htmlToShow.='</table>';
+      return $this->render('PRVitrineBundle:Vitrine:gallerie.html.twig', array(
+              'tabResult' => $htmlToShow
+      ));
   }
 
   public function contactsAction()
@@ -31,9 +94,9 @@ class VitrineController extends Controller
     return new Response($content);
   }
 
-  public function presentationAction()
+  public function uploadAction()
   {
-    $content = $this->get('templating')->render('PRVitrineBundle:Vitrine:presentation.html.twig');
+    $content = $this->get('templating')->render('PRVitrineBundle:Upload:presentation.html.twig');
 
     return new Response($content);
   }
