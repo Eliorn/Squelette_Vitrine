db1418895fd6f65de34a192b2fc00d0e37053982
Pierre Roumieux <p.roumieux87@gmail.com>
2018-06-05 23:14:00 +0200

    Accueil en BDD

---
+++ src/PR/VitrineBundle/Entity/Accueil.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace PR\VitrineBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Accueil
+ *
+ * @ORM\Table(name="accueil")
+ * @ORM\Entity(repositoryClass="PR\VitrineBundle\Repository\AccueilRepository")
+ */
+class Accueil
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="Contenu", type="text")
+     */
+    private $contenu;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set contenu
+     *
+     * @param string $contenu
+     *
+     * @return Accueil
+     */
+    public function setContenu($contenu)
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    /**
+     * Get contenu
+     *
+     * @return string
+     */
+    public function getContenu()
+    {
+        return $this->contenu;
+    }
+}
+
