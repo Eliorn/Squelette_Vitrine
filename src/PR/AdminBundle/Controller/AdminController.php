0ab434c93350974ae12253215844eaba86cb152f
Pierre Roumieux <p.roumieux87@gmail.com>
2018-06-05 21:51:21 +0200

    Debug Routing Admin

--- src/PR/AdminBundle/Controller/AdminController.php
+++ src/PR/AdminBundle/Controller/AdminController.php
@@ -5,7 +5,7 @@ namespace PR\AdminBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-class DefaultController extends Controller
+class AdminController extends Controller
 {
     /**
      * @Route("/")
