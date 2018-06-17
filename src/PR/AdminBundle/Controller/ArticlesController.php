c87fe1d80956233aeeba4a404fc94b34ab4fa7f7
Pierre Roumieux <p.roumieux87@gmail.com>
2018-06-17 22:06:24 +0200

    Admin : Gestion Articles : CRUD OK

--- src/PR/AdminBundle/Controller/ArticlesController.php
+++ src/PR/AdminBundle/Controller/ArticlesController.php
@@ -8,8 +8,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Forms;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use PR\VitrineBundle\Entity\Article;
 
 class ArticlesController extends Controller
 {
@@ -31,33 +33,82 @@ class ArticlesController extends Controller
 
     }
 
-    public function articleNewAction()
+    public function articleNewAction(Request $request)
     {
       $em = $this->getDoctrine()->getManager();
       $articlesRepository = $em->getRepository('PRVitrineBundle:Article');
+      $article= new Article();
 
-      $queryListArticles = $articlesRepository->createQueryBuilder('a')
-                                              ->orderBy('a.date','DESC');
-      $query = $queryListArticles->getQuery();
-      $listArticles = $query->getResult();
-      return $this->render('PRAdminBundle:Admin:article.html.twig', array(
-                'listArticles' => $listArticles,
-              )
+      if ($request->request->get('action')== 'Valider'){
+        $content = $request->request->get('form')['Content'];
+        $title = $request->request->get('form')['Title'];
+        if (isset($request->request->get('form')['Published'])){
+            $published = true;
+        }else{
+            $published = false;          
+        }
+        $user = $this->get('security.token_storage')->getToken()->getUser()->getUsername();
+        $date= new \DateTime();
+
+        $article->setContent($content);
+        $article->setTitle($title);
+        $article->setDate($date);
+        $article->setEditor($user);
+        $article->setPublished($published);
+
+        $em->persist($article);
+        $em->flush();
+        $request->getSession()->getFlashBag()->add('success', "La création de l'article a été effectuée");
+      }else if ($request->request->get('action')== 'Annuler'){
+        $request->getSession()->getFlashBag()->add('warn', "Retour au début !");
+      }
+
+      $formBuilder = $this->createFormBuilder($article)
+                          ->add('Title',    TextType::Class , array('empty_data' => 'Votre titre ici'))
+                          ->add('Content',  CKEditorType::Class , array('empty_data' => 'Contenu de l\'article'))
+                          ->add('Published', CheckboxType::Class, array('required' => false));
+
+      $form= $formBuilder->getForm();
+
+      return $this->render('PRAdminBundle:Admin:article_new.html.twig', array(
+                'form' => $form->createView(),
+            )
       );
 
     }
 
-    public function articleEditAction()
+    public function articleEditAction($id, Request $request )
     {
       $em = $this->getDoctrine()->getManager();
       $articlesRepository = $em->getRepository('PRVitrineBundle:Article');
+      $article= $articlesRepository->find($id);
 
-      $queryListArticles = $articlesRepository->createQueryBuilder('a')
-                                              ->orderBy('a.date','DESC');
-      $query = $queryListArticles->getQuery();
-      $listArticles = $query->getResult();
-      return $this->render('PRAdminBundle:Admin:article.html.twig', array(
-                'listArticles' => $listArticles,
+      if ($request->request->get('action')== 'Valider'){
+        $content = $request->request->get('form')['Content'];
+        $title = $request->request->get('form')['Title'];
+        $user = $this->get('security.token_storage')->getToken()->getUser()->getUsername();
+        $date= new \DateTime();
+        $article->setContent($content);
+        $article->setTitle($title);
+        $article->setDate($date);
+        $article->setEditor($user);
+
+        $em->persist($article);
+        $em->flush();
+        $request->getSession()->getFlashBag()->add('success', "La mise a jour de l'article a été effectuée");
+      }else if ($request->request->get('action')== 'Annuler'){
+        $request->getSession()->getFlashBag()->add('warn', "Retour au début !");
+      }
+
+      $formBuilder = $this->createFormBuilder($article)
+                          ->add('Title',    TextType::Class)
+                          ->add('Content',  CKEditorType::Class);
+
+      $form= $formBuilder->getForm();
+
+      return $this->render('PRAdminBundle:Admin:article_edit.html.twig', array(
+                'form' => $form->createView(),
+                'article_id' => $id
               )
       );
 
