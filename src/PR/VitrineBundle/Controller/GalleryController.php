8fb44717ac56cc610aaaea84213c3a89d4b2d79c
Socle , MY DESK <pierre.roumieux@umontpellier.fr>
2019-11-05 14:16:56 +0100

    Gestion de l'ordre des galeries

--- src/PR/VitrineBundle/Controller/GalleryController.php
+++ src/PR/VitrineBundle/Controller/GalleryController.php
@@ -23,7 +23,7 @@ class GalleryController extends Controller
 
     $queryListGalleries = $galleriesRepository->createQueryBuilder('a')
                           ->where('a.id > -1')
-                          ->orderBy('a.title','ASC');
+                          ->orderBy('a.galleryorder','ASC');
     $query = $queryListGalleries->getQuery();
     $listGalleries = $query->getResult();
 
